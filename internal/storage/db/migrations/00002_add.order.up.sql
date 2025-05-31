931ba25f0cb5025606e5d4883264247edf57621b
Timur <timur@MacBook-Pro-Timur.local>
2025-05-31 16:11:52 +0300

    fix migrations

--- internal/storage/db/migrations/00002_add.order.up.sql
+++ internal/storage/db/migrations/00002_add.order.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS "order"(
     id          VARCHAR(30) PRIMARY KEY ,
     status      VARCHAR(20) NOT NULL,
     user_id     INT NOT NULL,
-     balance     DOUBLE PRECISION NOT NULL,
+     accrual     DOUBLE PRECISION NOT NULL,
     uploaded_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now() 
 );
